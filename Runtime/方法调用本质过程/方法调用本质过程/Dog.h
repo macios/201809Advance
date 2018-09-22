1d1f09bfd103e7dfb334dd723a8f1054a07e7740
ac-hu <hunanlinmac@163.com>
2018-09-22 18:06:58 +0800

    1

---
+++ Runtime/方法调用本质过程/方法调用本质过程/Dog.h
@@ -0,0 +1,14 @@
+//
+//  Dog.h
+//  Runtime原理
+//
+//  Created by ac-hu on 2018/9/22.
+//  Copyright © 2018年 ac-hu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Dog : NSObject
+-(void)walk;
+-(void)run;
+@end
---
+++ Runtime/方法调用本质过程/方法调用本质过程/Dog.m
@@ -0,0 +1,18 @@
+//
+//  Dog.m
+//  Runtime原理
+//
+//  Created by ac-hu on 2018/9/22.
+//  Copyright © 2018年 ac-hu. All rights reserved.
+//
+
+#import "Dog.h"
+
+@implementation Dog
+-(void)walk{
+    NSLog(@"%s",__func__);
+}
+-(void)run{
+    NSLog(@"%s",__func__);
+}
+@end
