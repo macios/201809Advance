c6f2433bf4230bf8bfb7750a48b15a5e7c1b445a
ac-hu <hunanlinmac@163.com>
2018-09-23 11:21:15 +0800

    111

--- RunLoop/RunLoop相关结构体<类>(五个)/RunLoop相关结构体<类>(五个)/ViewController.m
+++ RunLoop/RunLoop相关结构体<类>(五个)/RunLoop相关结构体<类>(五个)/ViewController.m
@@ -16,13 +16,24 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-//    1.CFRunLoopRef;
+//    1.CFRunLoopRef;__CFRunLoop结构体
 //    2.CFRunLoopModeRef;
 //    3.CFRunLoopSourceRef;
 //    4.CFRunLoopTimerRef;
 //    5.CFRunLoopObserverRef;
     
-//    一个RunLoop 有多个mode，一个mode下有多个source、observe、timer；
+//    一个RunLoop管理者 管理多个mode，一个mode下有多个<set>source、<arr>observe、<arr>timer；
+    
+    CFRunLoopRef r1 = CFRunLoopGetCurrent();
+    CFRunLoopMode mode = CFRunLoopCopyCurrentMode(r1);
+    NSLog(@"mode = %@",mode);
+    
+    CFArrayRef arr = CFRunLoopCopyAllModes(r1);
+    NSLog(@"AllModes = %@",arr);
+//    UITrackingRunLoopMode,//界面滑动模式。
+//    GSEventReceiveRunLoopMode,//接收系统事件、用不上
+//    kCFRunLoopDefaultMode,//默认模式
+//    kCFRunLoopCommonModes//共用模式：前面的所有模式
 }
 
 
